95ef2bd1a645595aa99a5f75ae9590874be81f1b
Todd Minehardt <todd@todd-Precision-WorkStation-490.(none)>
2015-06-08 16:20:25 -0500

    Test 2.

--- Watershed_Master_Plan_2012_GIS_Analysis/python_scripts/Projects_Task.py
+++ Watershed_Master_Plan_2012_GIS_Analysis/python_scripts/Projects_Task.py
@@ -5,6 +5,7 @@ import os
 
 # Specify directory paths to the project components.
 # Test.
+# Test 2.
 gdb_name = 'Watershed_GIS_Analysis.gdb'
 project_dir = 'Watershed_Master_Plan_GIS_Analysis'
 root_dir = 'G:\WPDR_Common\Planning\Minehardt\Projects'
